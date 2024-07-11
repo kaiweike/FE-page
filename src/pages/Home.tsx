b746a186bb152c1943133c4c4b16aeaf14c6abdf
kaiweike <kaiweike.tw@gmail.com>
2024-07-11 11:30:35 +0800

    Connect API to Search page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,29 @@
+import { useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import NavBar from '../components/NavBar.tsx';
 import SearchPage from './SearchPage.tsx';
+import Result from './Result.tsx';
 import Profile from '../components/Profile.tsx';
 
 function Home() {
+  const [resultData, setResultData] = useState(null);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const handleSearch = async (keyword, pageSize) => {
+    navigate(`/result?&pageSize=${pageSize}&keyword=${keyword}`);
+  };
+
   return (
     <>
       <div className="flex justify-between">
         <NavBar />
         <div className="flex flex-1 justify-center">
           <div>
-            <SearchPage />
+            {location.pathname === '/' && (
+              <SearchPage onSearch={handleSearch} />
+            )}
+            {location.pathname === '/result' && <Result />}
           </div>
         </div>
         <div className="w-375 bg-bg-second hidden h-screen 2xl:block">
--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,5 +1,34 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+
 function Result() {
-  return <h1>Result Page</h1>;
+  const [data, setData] = useState(null);
+  const query = new URLSearchParams(useLocation().search); // get URL query string
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const pageSize = query.get('pageSize');
+      const keyword = query.get('keyword');
+      const response = await fetch(
+        `https://avl-frontend-exam.herokuapp.com/api/users/all?page=1&pageSize=${pageSize}&keyword=${keyword}`
+      );
+      const result = await response.json();
+      setData(result);
+    };
+
+    fetchData();
+  }, []);
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>Result Page</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
 }
 
 export default Result;
