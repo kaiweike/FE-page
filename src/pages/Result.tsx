0302ea69130fa089ac2d0ebe601b40f300d468e5
kaiweike <kaiweike.tw@gmail.com>
2024-07-11 22:31:59 +0800

    Layout Result page and implement responsive design

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import '../styles/Result.css';
 
 function Result() {
   const [data, setData] = useState(null);
@@ -24,10 +26,36 @@ function Result() {
   }
 
   return (
-    <div>
-      <h1>Result Page</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </div>
+    <>
+      <div className="2xl:flex">
+        <div className="result-navbar flex">
+          <Link to="/">
+            <img
+              src="src/assets/arrow.svg"
+              alt="Back Home"
+              className="result-back-arrow"
+            />
+          </Link>
+          <div className="result-homepage 2xl:hidden">Home Page</div>
+        </div>
+        <div>
+          <div className="result-results">Results</div>
+          <div className="result-picture grid-col-1 grid gap-x-1 gap-y-1 p-2 2xl:grid-cols-3">
+            <div>picture</div>
+            <div>picture</div>
+            <div>picture</div>
+            <div>picture</div>
+            <div>picture</div>
+            <div>picture</div>
+            <div>picture</div>
+            <div>picture</div>
+            <div>picture</div>
+          </div>
+          <button className="result-more">MORE</button>
+        </div>
+        <div className="result-spaceholder hidden 2xl:block" />
+      </div>
+    </>
   );
 }
 
