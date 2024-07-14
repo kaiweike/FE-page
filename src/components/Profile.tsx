ab71379926f9c3bb890a2ed4648de3c4f0aba476
kaiweike <kaiweike.tw@gmail.com>
2024-07-14 10:42:59 +0800

    Refine infinite scroll on Followers and Following
    - Separate scroll-triggered fetch from initial data fetch
    - Extract fetch function from useEffect into a standalone callback function
    - Add logic to prevent multiple fetches if already loading
    - Clear data and reset scroll position when switching between
      Followers/Following tabs

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,5 +1,5 @@
 import '../styles/Profile.css';
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useCallback } from 'react';
 import Followers from './Followers';
 import Following from './Following';
 import Refresh from '@mui/icons-material/Refresh';
@@ -8,14 +8,55 @@ function Profile() {
   const [activeButton, setActiveButton] = useState('followers');
   const [followersData, setFollowersData] = useState([]);
   const [followingData, setFollowingData] = useState([]);
-  const [loadNewData, setLoadNewData] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const scrollRef = useRef(null); // ref for the scroll container
+  const followersScrollRef = useRef(null); // ref for the followers scroll container
+  const followingScrollRef = useRef(null); // ref for the following scroll container
 
+  // fetch data function
+  const fetchData = useCallback(async () => {
+    if (loading) return;
+
+    setLoading(true);
+
+    try {
+      const baseUrl = `${import.meta.env.VITE_API_ENDPOINT}/users/`;
+      const userType = activeButton === 'followers' ? 'all' : 'friends';
+      const pageSize = activeButton === 'followers' ? 100 : 43;
+      const url = `${baseUrl}${userType}?page=1&pageSize=${pageSize}`;
+
+      const response = await fetch(url);
+      const newData = await response.json();
+
+      if (activeButton === 'followers') {
+        setFollowersData((prevData) => [...prevData, ...newData.data]);
+      } else {
+        setFollowingData((prevData) => [...prevData, ...newData.data]);
+      }
+    } catch (error) {
+      console.error('Error fetching followers/following:', error);
+    } finally {
+      setLoading(false);
+    }
+  }, [loading]);
+
+  // fetch initial data
   useEffect(() => {
-    async function fetchUsers() {
+    const resetScroll = () => {
+      if (activeButton === 'followers') {
+        if (followersScrollRef.current) {
+          followersScrollRef.current.scrollTop = 0;
+        }
+      } else {
+        if (followingScrollRef.current) {
+          followingScrollRef.current.scrollTop = 0;
+        }
+      }
+    };
+
+    const getData = async () => {
       setLoading(true);
+      resetScroll();
 
       try {
         const baseUrl = `${import.meta.env.VITE_API_ENDPOINT}/users/`;
@@ -27,28 +68,50 @@ function Profile() {
         const newData = await response.json();
 
         if (activeButton === 'followers') {
-          setFollowersData((prevData) => [...prevData, ...newData.data]);
+          setFollowersData([]);
+          setFollowersData(newData.data);
         } else {
-          setFollowingData((prevData) => [...prevData, ...newData.data]);
+          setFollowingData([]);
+          setFollowingData(newData.data);
         }
       } catch (error) {
         console.error('Error fetching followers/following:', error);
       } finally {
         setLoading(false);
-        setLoadNewData(false);
       }
-    }
+    };
 
-    fetchUsers();
-  }, [activeButton, loadNewData]);
+    getData();
+  }, [activeButton]);
 
-  // Scroll event handler
-  function handleScroll() {
-    const { scrollTop, scrollHeight, clientHeight } = scrollRef.current;
-    if (scrollTop + clientHeight >= scrollHeight - 5) {
-      setLoadNewData(true);
+  // handle the scroll event and call the fetchData function when the user reaches the end of the page
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollContainer =
+        activeButton === 'followers'
+          ? followersScrollRef.current
+          : followingScrollRef.current;
+      if (scrollContainer) {
+        const { scrollTop, scrollHeight, clientHeight } = scrollContainer;
+        if (scrollTop + clientHeight >= scrollHeight - 5) {
+          fetchData();
+        }
+      }
+    };
+
+    const scrollContainer =
+      activeButton === 'followers'
+        ? followersScrollRef.current
+        : followingScrollRef.current;
+    if (scrollContainer) {
+      scrollContainer.addEventListener('scroll', handleScroll);
     }
-  }
+    return () => {
+      if (scrollContainer) {
+        scrollContainer.removeEventListener('scroll', handleScroll);
+      }
+    };
+  }, [fetchData]);
 
   function LoadingAnimation() {
     return <Refresh className="animate-spin" />;
@@ -71,9 +134,10 @@ function Profile() {
         </button>
       </div>
       <div
-        ref={scrollRef}
-        onScroll={handleScroll}
-        className="hide-scrollbar h-[92%] overflow-y-scroll overscroll-y-contain pt-6"
+        ref={
+          activeButton === 'followers' ? followersScrollRef : followingScrollRef
+        }
+        className="hide-scrollbar h-[92%] overflow-y-scroll overscroll-y-none pt-6"
       >
         {activeButton === 'followers' ? (
           <Followers followers={followersData} />
